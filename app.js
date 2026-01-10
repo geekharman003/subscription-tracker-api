e3b00a7e22c74ad3a6fa09b53cbc0ccc812ca864
geekharman003 <harmansadhra2003@gmail.com>
2026-01-10 17:15:22 +0530

    added the global error middleware

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 import "dotenv/config";
-import connectToDatabase from "./database/mongodb.js";
 
-import express from "express";
+import express, { urlencoded } from "express";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import userRouter from "./routes/user.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// parse json data
+app.use(express.json());
+
+// parse url encoded data
+app.use(urlencoded({ extended: false }));
+
+// parse cookie data
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/users", userRouter);
+
+
+// global error middleware
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("home page");
 });
 
 app.listen(process.env.PORT || 3000, async () => {
-    console.log(`server is running on port ${process.env.PORT}`);
-    await connectToDatabase();
+  console.log(`server is running on port ${process.env.PORT}`);
+  await connectToDatabase();
 });
---
+++ middlewares/error.middleware.js
@@ -0,0 +1,36 @@
+const errorMiddleware = (err, req, res, next) => {
+  try {
+    let error = { ...err };
+    error.message = err.message;
+
+    console.error(err);
+
+    //Mongoose bad ObjectId
+    if (err.name === "CastError") {
+      error = new Error("Resource not found");
+      error.statusCode = 404;
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+      error = new Error("Duplicate field value entered");
+      error.statusCode = 400;
+    }
+
+    // Mongoose validation error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((val) => val.message);
+      error = new Error(message.join(","));
+      error.statusCode = 400;
+    }
+
+    res.status(error.statusCode || 500).join({
+      success: false,
+      message: error.message,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default errorMiddleware;
